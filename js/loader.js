a0215e1c401df733e69cc687d38982631f0b4d84
marcocomelon <mark.castro@acescredit.ph>
2026-01-12 14:58:29 +0800

    remove delay

--- js/loader.js
+++ js/loader.js
@@ -2,9 +2,8 @@
         window.addEventListener("load", () => {
         const loader = document.getElementById("loader");
         if (loader) {
-            // Delay in milliseconds (1000ms = 1s)
             setTimeout(() => {
                 loader.style.display = "none";
-            }, 1000);
+            });
         }
 });
